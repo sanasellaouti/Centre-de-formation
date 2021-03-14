493b48c1076c01e84d64d87a10e41e601002eb67
sanasellaouti <80283076+sanasellaouti@users.noreply.github.com>
2021-03-14 18:52:28 +0100

    Update principalInterface.py

--- principalInterface.py
+++ principalInterface.py
@@ -6,6 +6,8 @@ Created on Thu Mar 11 15:22:48 2021
 """
 from formateurGUI import *
 from certificationGUI import *
+from Interface Apprenant import *
+from Interface Formation import *
 from tkinter import *
 class PrincipalApp(tk.Tk):
     def __init__(self): 
@@ -20,13 +22,29 @@ class PrincipalApp(tk.Tk):
         command=self.gestion_certification)
         self.btn_formateur.place(x=100, y=100, height=25, width=200)
         self.btn_certification.place(x=100, y=150, height=25, width=200)
-    
+        #bouton gestion apprenant
+        self.btn_apprenant = tk.Button(self, text="Gestion apprenants", font=("Helvetica", 11), bg="yellow", fg="blue",  
+        command=self.gestion_apprenant)
+        #bouton gestion formation
+        self.btn_formation = tk.Button(self, text="Gestion formations", font=("Helvetica", 11), bg="yellow", fg="blue",  
+        command=self.gestion_formation)
+        self.btn_apprenant.place(x=100, y=200, height=25, width=200)
+        self.btn_formation.place(x=100, y=250, height=25, width=200)
+        
+  
     def gestion_formateur(self):
         app = FormateurApp()  
         app.mainloop()
     def gestion_certification(self):
         app = CertificationApp()  
         app.mainloop()
+    def gestion_apprenant(self):
+        app = ApprenantApp()  
+        app.mainloop()
+    def gestion_formation(self):
+        app = FormationApp()  
+        app.mainloop()
+    
 if __name__ == "__main__":  
     app = PrincipalApp()  
     app.mainloop() 
